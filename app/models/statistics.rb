a2aefeb4d3e5a50aa8e33da5db1cac5b423de08f
Matt <matthieutc@yahoo.com>
2012-02-28 00:14:14 +0100

    fixed a bug in the stats where the last registration time was wrong -- made the alerts less red

--- app/models/statistics.rb
+++ app/models/statistics.rb
@@ -4,7 +4,7 @@ class Statistics
     stats[:number_of_users] = User.count
     stats[:number_of_submissions] = Submission.count
     stats[:number_of_submissions_this_week] = Submission.where(['submissions.created_at > ?', 1.week.ago]).count
-    stats[:time_of_last_registration] = User.order("created_at desc").first.created_at
+    stats[:time_of_last_registration] = User.unscoped.order("created_at desc").first.created_at
     stats
   end
 end
--- spec/models/statistics_spec.rb
+++ spec/models/statistics_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Statistics do
-  describe "all" do
+  describe "loading statistics" do
     it "should retrieve the bundle of statistics" do
       user = ObjectMother.create_user
       submission = ObjectMother.create_submission :user => user
@@ -9,6 +9,14 @@ describe Statistics do
       stats = Statistics.load
       stats[:number_of_users].should == 1
       stats[:number_of_submissions].should == 1
+    end
+	
+    it "should fetch the registration time of the last created user" do
+      user_alphabetically_first = ObjectMother.create_user :username => 'a'
+	  second_user = ObjectMother.create_user :username => 'b'
+
+      stats = Statistics.load
+      stats[:time_of_last_registration].should == second_user.created_at
     end
   end
 end
