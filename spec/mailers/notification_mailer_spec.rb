104d393046f31a2575618528da78e6722e10b3e4
Matt <matthieutc@yahoo.com>
2012-03-13 22:19:55 +0100

    fixed a bug in the notification mailer's comment url -- fixed some css

--- spec/mailers/notification_mailer_spec.rb
+++ spec/mailers/notification_mailer_spec.rb
@@ -1,7 +1,10 @@
 require 'spec_helper'
 
 describe NotificationMailer do
+  include ApplicationHelper
+
   it "should generate a notification email" do
+	#require File.expand_path("../../app/helpers/application_helper", __FILE__)
     ActionMailer::Base.default_url_options[:host] = 'localhost'
 	reply_notification = ObjectMother.create_reply_notification
 
@@ -9,7 +12,7 @@ describe NotificationMailer do
 
     mail.to[0].should == reply_notification.user.email
     mail.subject.should == "[Socialite] #{reply_notification.comment.user} replied to you"
-	url = comment_url(reply_notification.comment, :host => 'localhost')
+	url = link_to_comment(reply_notification.comment)
 	mail.encoded.include?(url).should == true
   end
 end
